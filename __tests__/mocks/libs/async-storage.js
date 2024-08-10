7886e2b406ad3d39b08136eba93fd3159760558e
Lissone <leonardo.lissonez@gmail.com>
2024-08-10 16:59:59 -0300

    feat(libs/asyncStorage): add tests with asyncStorage mock

---
+++ __tests__/mocks/libs/async-storage.js
@@ -0,0 +1,6 @@
+export * from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+
+// eslint-disable-next-line no-undef
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
+);
